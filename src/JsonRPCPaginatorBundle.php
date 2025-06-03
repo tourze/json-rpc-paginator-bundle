788375199bab52a461a622790082d0fb05ba6cd7
tourze <tourze@users.noreply.github.com>
2025-06-03 11:45:49 +0800

    feat(paginator): 添加 knp-paginator-bundle 依赖并配置

--- src/JsonRPCPaginatorBundle.php
+++ src/JsonRPCPaginatorBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\JsonRPCPaginatorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class JsonRPCPaginatorBundle extends Bundle
+class JsonRPCPaginatorBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+        ];
+    }
 }
